e3ce6b6d8fe68d47f75c6a90fbc398db2e063c36
William Jung <williamjung0130@gmail.com>
2024-05-11 16:31:11 +0900

    modified margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import Link from "next/link";
 
 
 import ImgCard from "@/components/ui/imgcard";
@@ -17,28 +11,28 @@ export default function Home() {
         src="/christmas.jpg"
         title="Merry Christmas" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem rhoncuLorem ipsum dolor sit arna s orci urna"
-        className="flex-auto basis-1/4 mx-1"
+        className="flex-auto basis-1/4 mx-0.5"
         url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/frankfrut_street.jpg"
         title="Frankfurt Street" 
         subtitle="Lorem ipsum dolor sit amet cLorem ipsum dolor sit amet crna onsectetur. Lorem rhoncus orci urna"
-        className="flex-auto basis-1/4 mx-1"
+        className="flex-auto basis-1/4 mx-0.5"
         url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/rudesheim.jpg"
         title="Rudesheim" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet conrci urna orem rhoncus orci urna"
-        className="flex-auto basis-1/4 mx-1"
+        className="flex-auto basis-1/4 mx-0.5"
         url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/rudesheim.jpg"
         title="Rudesheim" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem rhoncus orci urna Lorem ipsum d Lorem rhoncus orci urna "
-        className="flex-auto basis-1/4 mx-1"
+        className="flex-auto basis-1/4 mx-0.5"
         url="https://williamjung0130.substack.com/">
       </ImgCard>
       </div>
--- src/components/ui/imgcard.tsx
+++ src/components/ui/imgcard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 // TypeScript with React Functional Component
 interface ImgCardProps {
