d2ca326681790841f816817139bac804b0cc1526
William Jung <williamjung0130@gmail.com>
2024-05-11 16:17:39 +0900

    imgcard border/hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,51 +5,43 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+
 
 import ImgCard from "@/components/ui/imgcard";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center mt-6 w-full">
-      <div className="flex flex-row justify-center min-w-[900px] my-4 flex-wrap scale-90">
+      <div className="flex flex-row justify-center min-w-[1000px] my-4 flex-wrap scale-95 gap-4">
       <ImgCard 
         src="/christmas.jpg"
         title="Merry Christmas" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem rhoncuLorem ipsum dolor sit arna s orci urna"
-        className="flex-auto basis-1/4 mx-0.1">
+        className="flex-auto basis-1/4 mx-1"
+        url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/frankfrut_street.jpg"
         title="Frankfurt Street" 
         subtitle="Lorem ipsum dolor sit amet cLorem ipsum dolor sit amet crna onsectetur. Lorem rhoncus orci urna"
-        className="flex-auto basis-1/4 mx-0.1">
+        className="flex-auto basis-1/4 mx-1"
+        url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/rudesheim.jpg"
         title="Rudesheim" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet conrci urna orem rhoncus orci urna"
-        className="flex-auto basis-1/4 mx-0.1">
+        className="flex-auto basis-1/4 mx-1"
+        url="https://williamjung0130.substack.com/">
       </ImgCard>
       <ImgCard 
         src="/rudesheim.jpg"
         title="Rudesheim" 
         subtitle="Lorem ipsum dolor sit amet consectetur. Lorem rhoncus orci urna Lorem ipsum d Lorem rhoncus orci urna "
-        className="flex-auto basis-1/4 mx-0.1">
+        className="flex-auto basis-1/4 mx-1"
+        url="https://williamjung0130.substack.com/">
       </ImgCard>
       </div>
-      <Card className="mb-4 shadow-none border-zinc-950">
-        <CardContent>
-          {/* Content here can be updated or left empty depending on your needs */}
-        </CardContent>
-      </Card>
     </main>
   );
 }
--- src/components/ui/imgcard.tsx
+++ src/components/ui/imgcard.tsx
@@ -6,22 +6,25 @@ interface ImgCardProps {
   title: string;
   subtitle: string;
   className?: string;
+  url: string; // New prop for URL
 }
 
-const ImgCard: React.FC<ImgCardProps> = ({ src, title, subtitle, className}) => {
+const ImgCard: React.FC<ImgCardProps> = ({ src, title, subtitle, className, url}) => {
   return (
-    <div className={`relative w-full max-w-xs rounded-lg overflow-hidden mx-4 ${className} flex-auto`}>
-      <img
-        alt="Abstract art as background"
-        className="w-full h-full object-cover aspect-[1/1.3]" 
-        src={src}
-      />
-      <div style={{ background: 'linear-gradient(to top, rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0))' }} className="absolute inset-0" />
-      <div className="absolute bottom-0 left-0 p-6 text-white">
-        <h4 className="text-lg font-semibold">{title}</h4>
-        <h5 className="text-base text-gray-300 font-regular mt-1">{subtitle}</h5>
+    <a href={url} className="block"> {/* Wrap content with an anchor tag */}
+      <div className={`relative w-full max-w-xs rounded-lg overflow-hidden mx-4 ${className} flex-auto border border-zinc-800 transition-transform duration-300 ease-in-out hover:scale-105`}>
+        <img
+          alt="Abstract art as background"
+          className="w-full h-full object-cover aspect-[1/1.25]" 
+          src={src}
+        />
+        <div style={{ background: 'linear-gradient(to top, rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0))' }} className="absolute inset-0" />
+        <div className="absolute bottom-0 left-0 p-6 text-white">
+          <h4 className="text-lg font-semibold">{title}</h4>
+          <h5 className="text-base text-gray-300 font-regular mt-1">{subtitle}</h5>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
